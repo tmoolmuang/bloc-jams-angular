f25c23a6aeae08570887fe307a1c5c81a8d13c95
tmoolmuang <tmoolmuang@yahoo.com>
2017-02-21 20:24:46 -0800

    checkpoint05: completed

--- dist/scripts/app.js
+++ dist/scripts/app.js
@@ -7,7 +7,8 @@
 			});
 		$stateProvider
 			.state('landing', {
-				url: '/',
+				url: '/',        
+				controller: 'LandingCtrl as landing', 
 				templateUrl: '/templates/landing.html'
 			})
 			.state('album', {
@@ -16,6 +17,7 @@
 			})
 			.state('collection', {
 				url: '/collection',
+				controller: 'CollectionCtrl as collection',
 				templateUrl: '/templates/collection.html'
 			});
 	}
